bcdd489d9e8d7354e31c2f9221046c499a9e77ac
Radivoje <radivoje@magmadev.co>
2026-02-15 17:16:30 +0100

    .

--- src/Buhlmann.Zhl16c/Output/PlanResult.cs
+++ src/Buhlmann.Zhl16c/Output/PlanResult.cs
@@ -23,13 +23,41 @@ public struct PlanResult
     public override string ToString()
     {
         var stringBuilder = new StringBuilder();
-        var steps = SegmentCount;
+        var totalRuntimeSec = 0;
+        stringBuilder.AppendLine($"{"depth",8}  {"duration",10}  {"runtime",10}  {"gas",12}");
 
-        for (var i = 0; i < steps; i++)
+        for (var i = 0; i < SegmentCount; i++)
         {
-            stringBuilder.AppendLine(Segments[i].ToString());
+            ref var seg = ref Segments[i];
+
+            var startDepthM = seg.DepthStartMm / 1000;
+            var endDepthM = seg.DepthEndMm / 1000;
+            var durationSec = seg.RuntimeEndSec - seg.RuntimeStartSec;
+            totalRuntimeSec = seg.RuntimeEndSec;
+
+            var durationMin = (durationSec + 30) / 60;
+            var runtimeMin = (totalRuntimeSec + 30) / 60;
+
+            if (startDepthM != endDepthM)
+            {
+                if (startDepthM == 0 && endDepthM > 0)
+                {
+                    stringBuilder.AppendLine($"{endDepthM + "m",8}  {durationMin + "min",10}  {runtimeMin + "min",10}  EAN25");
+                }
+                else if (endDepthM == 0)
+                {
+                    stringBuilder.AppendLine($"{"0m",8}  {durationMin + "min",10}  {runtimeMin + "min",10}  EAN25");
+                }
+
+                continue;
+            }
+
+            stringBuilder.AppendLine($"{endDepthM + "m",8}  {durationMin + "min",10}  {runtimeMin + "min",10}  EAN25");
         }
 
+        stringBuilder.AppendLine();
+        stringBuilder.AppendLine($"Runtime: {(totalRuntimeSec + 30) / 60}min ({totalRuntimeSec}s)");
+        
         return stringBuilder.ToString();
     }
 }
